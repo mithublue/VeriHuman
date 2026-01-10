94549f534a25cfa450d2b3dbadff6a105c6caec5
Mithu <cemithu06@gmail.com>
2026-01-10 13:00:03 +0600

    module export fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
 import { Header } from '@/components/Header';
 import { HomeClient } from './HomeClient';
 
+// Force dynamic rendering - Header uses auth()
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
     return (
         <div className="min-h-screen flex flex-col bg-gray-50">
